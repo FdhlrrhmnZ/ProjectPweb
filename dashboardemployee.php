c173456f45f1fea876bf29aeee6738a6918d50d3
Hanief R <hanieframadhin@gmail.com>
2026-06-02 11:30:27 +0700

    navigasi pada tiap dashboard dan tombol regist pada login

--- dashboardemployee.php
+++ dashboardemployee.php
@@ -7,3 +7,13 @@ require "inc.koneksi.php";
 echo "Welcome, <b>". $_SESSION["name"]."</b><br>";
 echo "Anda login sebagai, <b>". $_SESSION["role"]."</b>";
 ?>
+
+<div id="navbar3" class="navbar-collapse collapse">
+    <ul class="nav navbar-nav">
+        <li><a href="dashboardemployee.php">Home</a></li>
+        <li><a href="dashboardemployee.php?page=profile">View Profile</a></li>
+        <li><a href="dashboardemployee.php?page=viewproject">View Project</a></li>
+        <li><a href="dashboardemployee.php?page=logout">Log Out</a></li>
+
+    </ul>
+</div>
