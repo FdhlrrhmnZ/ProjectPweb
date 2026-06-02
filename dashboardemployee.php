a0ee5ea8b556fd6165dab52f51c27f402b142767
Fadhlurrahman Zaini <fadhz2986@gmail.com>
2026-06-02 10:26:38 +0700

    isi dashboard

---
+++ dashboardemployee.php
@@ -0,0 +1,9 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+require "inc.koneksi.php";
+
+echo "Welcome, <b>". $_SESSION["name"]."</b><br>";
+echo "Anda login sebagai, <b>". $_SESSION["role"]."</b>";
+?>
